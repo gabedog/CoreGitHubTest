83e08bcf6a7da7754c48944ff928232f7b026dd5
gabedog <satch1917@live.com>
2017-10-28 17:26:09 -0500

    grabbing new code from Dev by Rebase

--- CoreGitHub/Program.cs
+++ CoreGitHub/Program.cs
@@ -10,6 +10,8 @@ namespace CoreGitHub
             Console.WriteLine("This is the dev branch check in....");
             Console.WriteLine("Adding to Feature 1....");
             Console.WriteLine("Adding to Feature 1 more code....");
+
+            Console.WriteLine("new code in dev meant for Feature 1");
             Console.ReadLine();
         }
     }
